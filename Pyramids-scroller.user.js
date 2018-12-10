cb15e76dee3039d85b80f58a12c7c9c9f9953bf5
6lancmange <lubiewachac.benzaldehyd@gmail.com>
2018-12-10 05:00:32 +0100

    Update Pyramids-scroller.user.js

--- Pyramids-scroller.user.js
+++ Pyramids-scroller.user.js
@@ -2,6 +2,7 @@
 // @name         Neopets – Pyramids scroller
 // @description  scrolls the Pyramids game down to the game
 // @match        http://www.neopets.com/games/pyramids/pyramids.phtml*
+// @match        http://www.neopets.com/games/sakhmet_solitaire/sakhmet_solitaire.phtml*
 // ==/UserScript==
 
 $(document).ready(function(){
