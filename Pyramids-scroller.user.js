adcae1423e271759d734188eb3217b984293c5a3
6lancmange <lubiewachac.benzaldehyd@gmail.com>
2018-12-11 03:28:09 +0100

    Update Pyramids-scroller.user.js
    For now this works well either for Pyramids or for Solitaire. At least for me because I'm not a fan of Solitaire getting scrolled that low, but if you are, then that's great! Tbh I wonder if others have the same preference as I do. I'm leaving the default as what it was before, i.e. scrolling to the game title bar.

--- Pyramids-scroller.user.js
+++ Pyramids-scroller.user.js
@@ -1,10 +1,14 @@
 // ==UserScript==
 // @name         Neopets – Pyramids scroller
-// @description  scrolls the Pyramids game down to the game
+// @description  scrolls the Pyramids and Solitaire games down to the game
 // @match        http://www.neopets.com/games/pyramids/pyramids.phtml*
 // @match        http://www.neopets.com/games/sakhmet_solitaire/sakhmet_solitaire.phtml*
 // ==/UserScript==
 
 $(document).ready(function(){
+    //use this if you want to scroll to the black bar with the title of the game
     document.getElementById('content').scrollIntoView()
+
+    //use this if you want to scroll to the top grey row of the table (starting with "consecutive plays", or "draws remaining")
+    //document.querySelector('.contentModule table').scrollIntoView()
 });
